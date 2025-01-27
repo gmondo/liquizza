719f1a79fa59a7982bd60e1203ffa4eba7ce3092
Giovanni Mondo <giovanni.mondo@telecomitalia.it>
2025-01-27 12:47:59 +0100

    first working release

--- liquizza.py
+++ liquizza.py
@@ -1,5 +1,6 @@
 import random
 import os
+import sys
 import time
 
 def elabora_contenuto_csv(nome_file):
@@ -24,8 +25,86 @@ def elabora_contenuto_csv(nome_file):
     return domande
 
 def elabora_contenuto_txt(nome_file):
-    domande = []
-    return domande
+    with open(nome_file, 'r', encoding='utf-8') as file:
+        righe = file.readlines()
+
+    domande_e_risposte = []
+    stato = 'domanda'
+    domanda = []
+    risposte_errate = []
+    risposte_corrette = []
+    testo_corrente = None
+    prev_righe_vuote = 0
+
+    def aggiungi_domanda():
+        """Aggiunge la domanda corrente alla lista finale, se valida."""
+        if domanda:
+            domande_e_risposte.append({
+                'domanda': ' '.join(domanda),
+                'num_risposte_errate': len(risposte_errate),
+                'risposte': risposte_errate + risposte_corrette,
+            })
+
+    for riga in righe:
+        print(stato, riga)
+        riga = riga.rstrip()  # Rimuove spazi e newline finali
+        if riga.lstrip().startswith('#'):  # Ignora commenti
+            continue
+
+        if riga.strip() == '':  # Riga vuota
+            prev_righe_vuote += 1
+            if prev_righe_vuote > 1:
+                continue
+
+            # Cambia stato in base al contesto
+            if stato == 'domanda':
+                stato = 'risposte_errate'
+            elif stato == 'risposte_errate':
+                if testo_corrente:
+                    risposte_errate.append(testo_corrente.rstrip())
+                testo_corrente = None
+                stato = 'risposte_corrette'
+            elif stato == 'risposte_corrette':
+                if testo_corrente:
+                    risposte_corrette.append(testo_corrente.rstrip())
+                testo_corrente = None
+                aggiungi_domanda()
+                stato = 'domanda'
+                domanda = []
+                risposte_errate = []
+                risposte_corrette = []
+            continue
+
+        prev_righe_vuote = 0
+
+        if stato == 'domanda':
+            domanda.append(riga+'\n')
+            continue
+
+        if stato in ['risposte_errate', 'risposte_corrette']:
+            # Se la riga è indentata, è una continuazione della risposta corrente
+            if riga[0].isspace():
+                if testo_corrente:
+                    testo_corrente += '\n' + riga.rstrip()
+                continue
+
+            # Salva la risposta corrente e inizia una nuova
+            if testo_corrente:
+                if stato == 'risposte_errate':
+                    risposte_errate.append(testo_corrente.rstrip())
+                elif stato == 'risposte_corrette':
+                    risposte_corrette.append(testo_corrente.rstrip())
+            testo_corrente = riga
+
+    # Salva l'ultima risposta e domanda, se presenti
+    if testo_corrente:
+        if stato == 'risposte_errate':
+            risposte_errate.append(testo_corrente.rstrip())
+        elif stato == 'risposte_corrette':
+            risposte_corrette.append(testo_corrente.rstrip())
+    aggiungi_domanda()
+
+    return domande_e_risposte
 
 def leggi_domande_da_file(nome_file):
     domande = []
@@ -33,19 +112,7 @@ def leggi_domande_da_file(nome_file):
         domande = elabora_contenuto_csv(nome_file)
     else:
         domande = elabora_contenuto_txt(nome_file)
-    return domande
-    with open(nome_file, "r") as file:
-        righe = file.readlines()
-        for riga in righe:
-            elementi = riga.strip().split(";")
-            domanda = elementi[0]
-            risposte = elementi[1:-1]
-            risposte_corrette = elementi[-1]
-            domande.append({
-                "domanda": domanda,
-                "risposte": risposte,
-                "risposte_corrette": risposte_corrette
-            })
+    print(domande)
     return domande
 
 def scegli_file_da_lista(files):
@@ -56,15 +123,19 @@ def scegli_file_da_lista(files):
     return files[int(scelta) - 1]
 
 def main():
-    directory = os.getcwd() 
-    files_csv = [file for file in os.listdir(directory) \
+    if len(sys.argv) > 1:
+        nome_file = sys.argv[1]
+    else:
+        directory = os.getcwd() 
+        file_choices = [file for file in os.listdir(directory) \
             if file.endswith(".csv") or file.endswith(".txt")]
     
-    if not files_csv:
-        print(f"Nessun file .csv o .txt trovato nella directory {directory}.")
-        return
+        if not file_choices:
+            print(f"No supported file in {directory}.")
+            return
     
-    nome_file = scegli_file_da_lista(files_csv)  # Scegli il file da caricare
+        nome_file = scegli_file_da_lista(file_choices)
+
     domande = leggi_domande_da_file(nome_file)
     random.shuffle(domande)  # Mischia l'ordine delle domande
 
